ed0db096af223a4e76c6ecd544b8920482d62508
twonkista <rkomati787@gmail.com>
2024-09-30 19:29:57 -0500

    name and main info

---
+++ portfolio-web/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+</script>
+
+<template>
+  <div class="title">
+        <h1 class="h1 is-3">Rohith Komati</h1>
+        <h1>Sophomore/Junior at University of Illinois - Chicago<br>Former Software Engineer Intern at Siemens</h1>
+    </div>
+</template>
+
+<style>
+.title {
+  text-align: left;
+  float: left;
+  color: Black;
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
+.h1{
+  font-size: 80px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.smalls{
+  font-size: 20px;
+  font-family: Arial, Helvetica, sans-serif, Thin;
+}
+</style>
