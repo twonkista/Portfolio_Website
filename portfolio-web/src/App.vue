00a423597b5a182db6f9175586b80e90ded92e8d
twonkista <rkomati787@gmail.com>
2024-09-30 20:00:22 -0500

    made the splash screen without buttons

--- portfolio-web/src/App.vue
+++ portfolio-web/src/App.vue
@@ -2,20 +2,31 @@
 </script>
 
 <template>
-  <div class="title">
-        <h1 class="h1 is-3">Rohith Komati</h1>
-        <h1>Sophomore/Junior at University of Illinois - Chicago<br>Former Software Engineer Intern at Siemens</h1>
+  <Transition appear>
+    <div class="title">
+          <h1 class="h1 is-3">Rohith Komati</h1>
+          <h1 class="smalls">Sophomore/Junior at University of Illinois - Chicago<br>Former Software Engineer Intern at Siemens</h1>
     </div>
+  </Transition>
 </template>
 
 <style>
+body {
+  background: url("@/assets/bg_image.jpg") no-repeat center center fixed;
+    -moz-background-size: cover;
+    -webkit-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
 .title {
+  position: absolute;
   text-align: left;
   float: left;
-  color: Black;
+  color: White;
   margin: auto;
-  width: 50%;
-  padding: 10px;
+  width: 100%;
+  height: 100%;
+  padding: 5px;
 }
 .h1{
   font-size: 80px;
@@ -23,6 +34,17 @@
 }
 .smalls{
   font-size: 20px;
-  font-family: Arial, Helvetica, sans-serif, Thin;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 2s ease, transform 1s ease-in-out;
+  transform: translateY(0px);
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(100px);
 }
 </style>
