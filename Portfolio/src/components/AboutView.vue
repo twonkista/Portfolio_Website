94ef87a2e03f7df8179714ed0b71d91941381200
twonkista <rkomati787@gmail.com>
2024-10-07 17:47:42 -0500

    changes image import

--- Portfolio/src/components/AboutView.vue
+++ Portfolio/src/components/AboutView.vue
@@ -4,13 +4,13 @@ import { ref } from 'vue'
 
 <template>
   <div class = "image">
-      <img alt="Me" class="logo" src="@/assets/me.jpg" width="125" height="125"/>
+      <img alt="Me" class="logo" src="../assets/me.jpg" width="125" height="125"/>
     </div>
     <a href="https://github.com/twonkista" target="_blank">
-      <img src="@/assets/github_logo.jpg" width="60" height="60"/>
+      <img src="../assets/github_logo.jpg" width="60" height="60"/>
     </a>
     <a href="https://www.linkedin.com/in/rohith-k-b24104284" target="_blank">
-      <img src="@/assets/linkedin.png" width="60" height="60"/>
+      <img src="../assets/linkedin.png" width="60" height="60"/>
     </a>
     <br>
     <p>
@@ -20,32 +20,32 @@ import { ref } from 'vue'
     <header class = "divider">Skills Section: </header>
     <header>Languages: </header>
     <div class="imagegroup">
-      <img src="@/assets/c_logo.png" width="60" height="60">
-      <img src="@/assets/c++_logo.png" width="60" height="60">
-      <img src="@/assets/java-logo-1.png" width="60" height="60" class="java">
-      <img src="@/assets/javaScript-logo.png" width="60" height="60">
-      <img src="@/assets/typescript_logo_2020.svg.png" width="60" height="60">
-      <img src="@/assets/images (1).jpeg" width="60" height="60">
-      <img src="@/assets/postgresql_elephant.svg.png" width="60" height="60">
-      <img src="@/assets/sql_data_base_with_logo.png" width="120" height="60" class="java">
+      <img src="../assets/c_logo.png" width="60" height="60">
+      <img src="../assets/c++_logo.png" width="60" height="60">
+      <img src="../assets/java-logo-1.png" width="60" height="60" class="java">
+      <img src="../assets/javaScript-logo.png" width="60" height="60">
+      <img src="../assets/typescript_logo_2020.svg.png" width="60" height="60">
+      <img src="../assets/images (1).jpeg" width="60" height="60">
+      <img src="../assets/postgresql_elephant.svg.png" width="60" height="60">
+      <img src="../assets/sql_data_base_with_logo.png" width="120" height="60" class="java">
     </div>
     <header>Frameworks: </header>
     <div class="imagegroup">
-      <img src="@/assets/images (1).png" width="60" height="60">
-      <img src="@/assets/images (3).png" width="60" height="60" class="java">
-      <img src="@/assets/pyrorch_logo_black.svg.png" width="200" height="60" class="java">
-      <img src="@/assets/images (2).png" width="60" height="60">
-      <img src="@/assets/flask.png" width="60" height="60" class="java">
+      <img src="../assets/images (1).png" width="60" height="60">
+      <img src="../assets/images (3).png" width="60" height="60" class="java">
+      <img src="../assets/pyrorch_logo_black.svg.png" width="200" height="60" class="java">
+      <img src="../assets/images (2).png" width="60" height="60">
+      <img src="../assets/flask.png" width="60" height="60" class="java">
     </div>
     <header>Libraries/Tools: </header>
     <div class="imagegroup">
-      <img src="@/assets/images (4).png" width="100" height="60">
-      <img src="@/assets/images (5).png" width="160" height="60">
-      <img src="@/assets/images (6).png" width="60" height="60">
-      <img src="@/assets/images (2).png" width="60" height="60">
-      <img src="@/assets/97_docker_logo_logos-512.png" width="60" height="60" class="java">
-      <img src="@/assets/vmware_cloud_logo.jpg" width="80" height="60" class="java">
-      <img src="@/assets/tux.svg.png" width="60" height="60">
+      <img src="../assets/images (4).png" width="100" height="60">
+      <img src="../assets/images (5).png" width="160" height="60">
+      <img src="../assets/images (6).png" width="60" height="60">
+      <img src="../assets/images (2).png" width="60" height="60">
+      <img src="../assets/97_docker_logo_logos-512.png" width="60" height="60" class="java">
+      <img src="../assets/vmware_cloud_logo.jpg" width="80" height="60" class="java">
+      <img src="../assets/tux.svg.png" width="60" height="60">
     </div>
 </template>
 
